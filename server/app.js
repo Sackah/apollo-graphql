9626092f1a7d41854185f639cac4156434ca801e
Sackah <yeitsam5@gmail.com>
2024-03-12 09:00:36 +0000

    step: setup server

---
+++ server/app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const { graphqlHTTP } = require("express-graphql");
+const schema = require("./schema/schema");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+mongoose
+    .connect(process.env.DB_URI)
+    .then((result) => {
+        console.log("db connected");
+        app.listen(4000, "0.0.0.0");
+    })
+    .catch((err) => {
+        console.log(err.message);
+    });
+
+const app = express();
+
+app.use(
+    "/graphql",
+    graphqlHTTP({
+        schema,
+        graphiql: true,
+    })
+);
+
+app.listen(4000, () => {
+    console.log("now listenening for requests on port 4000");
+});
